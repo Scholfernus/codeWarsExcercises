d73cf060f0f080c5dab259e2f146c2cfa1b9a4e9
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-05-09 22:32:51 +0200

    Chat GPT Exc 5 długość wyrazu sortowanie 09.05.2023

---
+++ src/main/java/org/example/chatGPTzadania/Exc5_Dlugosc_Wyrazu.java
@@ -0,0 +1,25 @@
+package org.example.chatGPTzadania;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exc5_Dlugosc_Wyrazu {
+    //Napisz program, który pobiera od użytkownika listę napisów i sortuje je według długości napisów.
+    public static void main(String[] args) {
+        Scanner inputWords = new Scanner(System.in);
+        List<String> countWordLength = new ArrayList<>();
+        List<Integer>wordLength = new ArrayList<>();
+        System.out.println("Wpisz listę 6 wyrazów: ");
+        for (int i = 0; i < 6; i++) {
+            String word = inputWords.nextLine();
+            countWordLength.add(word);
+            wordLength.add(word.length());
+        }
+        Collections.sort(wordLength);
+        for (var v : wordLength ) {
+            System.out.println(v);
+        }
+    }
+}
