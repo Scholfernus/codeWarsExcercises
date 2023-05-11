42ab6555ccdc05e142e73cfcd876e30e70932a6e
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-05-11 17:52:01 +0200

    Chat GPT Exc Bankomat, SprawdzanieSalda, wpłata 11.05.2023

---
+++ src/main/java/org/example/chatGPTzadania/SumaLiczb.java
@@ -0,0 +1,25 @@
+package org.example.chatGPTzadania;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class SumaLiczb {
+    public static void main(String[] args) {
+        System.out.println("Podaj dowolną liczbę kilkucyfrową, a policzę sumę liczb: ");
+        Scanner input = new Scanner(System.in);
+        int suma = 0;
+        try{
+
+            int number = input.nextInt();
+            String s = String.valueOf(number);
+            char[] array = s.toCharArray();
+            for (var elem : array) {
+                int value = Character.getNumericValue(elem);
+                suma +=value;
+            }
+            System.out.println(suma);
+        }catch (InputMismatchException e){
+            System.out.println("Błąd: Podana wartość nie jest liczbą całkowitą.");
+        }
+    }
+}
