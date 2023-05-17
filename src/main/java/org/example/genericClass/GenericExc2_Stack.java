32022cf6ae98de477dd3b985e8c7fd3d5f725192
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-05-17 13:44:18 +0200

    GenericExc2_Stack 17.05.2023

--- src/main/java/org/example/genericClass/GenericExc2_Stack.java
+++ src/main/java/org/example/genericClass/GenericExc2_Stack.java
@@ -9,18 +9,18 @@ import java.util.List;
 // Metoda pop() powinna usuwać i zwracać element ze szczytu stosu. Jeśli stos jest pusty, metoda powinna zwrócić wartość null.
 // Metoda peek() powinna zwracać element ze szczytu stosu, ale nie powinna go usuwać. Jeśli stos jest pusty, metoda powinna zwrócić wartość null.
 // Metoda isEmpty() powinna zwracać wartość true, jeśli stos jest pusty, i wartość false, jeśli na stosie znajdują się jakieś elementy.
- public class GenericExc2_Stack<T> {
+public class GenericExc2_Stack<T> {
     private List<T> stack = new ArrayList<>();
     private boolean isEmpty = true;
+
     public static void main(String[] args) {
-    GenericExc2_Stack<Integer>stack = new GenericExc2_Stack<>();
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-
-
-
-
+        GenericExc2_Stack<Integer> stack = new GenericExc2_Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.isEmpty());
     }
 
     void push(T element) {
@@ -28,20 +28,26 @@ import java.util.List;
         isEmpty = false;
     }
 
-    void pop() {
-        ;
-    }
-
-    void peek() {
-
-    }
-
-    void isEmpty() {
-        if (stack.isEmpty()) {
-            boolean noArgs = isEmpty;
-        } else {
-            boolean full;
+    T pop() {
+        if (!isEmpty) {
+            T element = stack.remove(stack.size() - 1);
+            if (stack.isEmpty()) {
+                isEmpty = true;
+            }
+            return element;
         }
+        return null;
+    }
+
+    T peek() {
+        if (!isEmpty()) {
+            return stack.get(stack.size() - 1);
+        }
+        return null;
+    }
+
+    boolean isEmpty() {
+        return stack.isEmpty();
     }
 
 }
