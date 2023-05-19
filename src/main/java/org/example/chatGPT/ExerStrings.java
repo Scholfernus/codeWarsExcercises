598d24b4a4e945cfaeab18a829399668f768c8ab
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-05-19 21:40:08 +0200

    ChaGPT phrase toUpperCase 19.05.2023

--- src/main/java/org/example/chatGPT/ExerStrings.java
+++ src/main/java/org/example/chatGPT/ExerStrings.java
@@ -4,15 +4,24 @@ import java.util.Scanner;
 
 public class ExerStrings {
     public static void main(String[] args) {
+        ExerStrings exercise = new ExerStrings();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Napisz dowolne zdanie, obliczę jego długość.");
         String nextLine = scanner.nextLine();
-        ExerStrings exercise = new ExerStrings();
+
         int i = exercise.writingLength(nextLine);
         System.out.printf("Zdanie ma %d znaki.",i);
+
+        System.out.println("Napisz kolejne zdanie, zmienię jego litery na wielkie.");
+        String letterUp = exercise.letterUp(nextLine);
+        System.out.println("Zdanie z wielkimi literami: " + letterUp);
     }
     public int writingLength(String text){
         int length = text.length();
         return length;
+    }
+    public String letterUp(String text){
+        String toUpperCase = text.toUpperCase();
+        return toUpperCase;
     }
 }
