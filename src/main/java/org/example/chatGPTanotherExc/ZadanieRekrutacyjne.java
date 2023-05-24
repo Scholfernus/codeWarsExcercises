1b91c5983928260e4cdce9d132c5a27ab2f2e4c3
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-05-24 22:03:51 +0200

    Sort 24.05.2023

--- src/main/java/org/example/chatGPTanotherExc/ZadanieRekrutacyjne.java
+++ src/main/java/org/example/chatGPTanotherExc/ZadanieRekrutacyjne.java
@@ -1,6 +1,8 @@
 package org.example.chatGPTanotherExc;
 
 import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
 
 public class ZadanieRekrutacyjne {
     public static void main(String[] args) {
@@ -10,9 +12,24 @@ public class ZadanieRekrutacyjne {
         Arrays.sort(numbers);
         System.out.println(Arrays.toString(numbers));
         System.out.println(Arrays.toString(sorted(numbers)));
+        IntStream intStream = Arrays.stream(numbers);
+        intStream.forEach(System.out::print);
+        System.out.println(Arrays.toString(new OptionalInt[]{min(numbers)}));
+        System.out.println(Arrays.toString(new OptionalInt[]{max(numbers)}));
+
+
+
     }
 
     public static int[] sorted(int[] sort) {
         return Arrays.stream(sort).sorted().toArray();
+    }
+    public static OptionalInt min(int[]array){
+        OptionalInt min = Arrays.stream(array).min();
+        return min;
+    }
+    public static OptionalInt max(int[]array){
+        OptionalInt max = Arrays.stream(array).max();
+        return max;
     }
 }
