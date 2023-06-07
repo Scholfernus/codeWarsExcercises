4dff1855593dc4cf24345b75ffc91f3a6308ebe7
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-07 20:23:43 +0200

    AverageInteger

---
+++ src/main/java/org/example/chatGPTzadania/Methods/AverageInteger.java
@@ -0,0 +1,18 @@
+package org.example.chatGPTzadania.Methods;
+
+public class AverageInteger {
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 6, 8, 10};
+        double average = calculateAverage(numbers);
+        System.out.println("Średnia: " + average);
+    }
+
+    public static double calculateAverage(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        double result = (double) sum / numbers.length;
+        return result;
+    }
+}
