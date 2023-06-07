5e16e4c66f6b2004221017ff4338ae26711fdefc
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-07 20:24:23 +0200

    Interfejs Koło i prostokąt

---
+++ src/main/java/org/example/chatGPTzadania/Methods/Shape.java
@@ -0,0 +1,59 @@
+package org.example.chatGPTzadania.Methods;
+
+public interface Shape {
+    double calculateArea();
+
+    double calculatePerimeter();
+}
+
+class Circle implements Shape {
+    private int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle implements Shape {
+    private int length;
+    private int width;
+
+    public Rectangle(int length, int width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5);
+        double circleArea = circle.calculateArea();
+        double circlePerimeter = circle.calculatePerimeter();
+        System.out.println("Circle area: " + circleArea);
+        System.out.println("Circle perimeter: " + circlePerimeter);
+
+        Rectangle rectangle = new Rectangle(8, 10);
+        double rectangleArea = rectangle.calculateArea();
+        double rectanglePerimeter = rectangle.calculatePerimeter();
+        System.out.println("Rectangle area: " + rectangleArea);
+        System.out.println("Rectangle perimeter: " + rectanglePerimeter);
+    }
+}
