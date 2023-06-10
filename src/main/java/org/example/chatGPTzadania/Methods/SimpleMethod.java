ea8352b247558f1c0362f26b7240393829a5a27a
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-10 23:27:36 +0200

    Prosta metoda z dodawaniem

---
+++ src/main/java/org/example/chatGPTzadania/Methods/SimpleMethod.java
@@ -0,0 +1,13 @@
+package org.example.chatGPTzadania.Methods;
+
+public class SimpleMethod {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = calculateSum(a, b);
+        System.out.println("Suma liczb " + a + " i " + b + " wynosi: " + sum);
+    }
+    public static int calculateSum(int num1, int num2){
+        return num1 + num2;
+    }
+}
