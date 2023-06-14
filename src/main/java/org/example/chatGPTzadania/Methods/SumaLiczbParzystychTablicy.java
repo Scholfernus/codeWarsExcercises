41ea38fd1845ac3c48426686bc243c20d1fac2e1
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-14 21:44:15 +0200

    Suma liczb parzystych tablicy

---
+++ src/main/java/org/example/chatGPTzadania/Methods/SumaLiczbParzystychTablicy.java
@@ -0,0 +1,17 @@
+package org.example.chatGPTzadania.Methods;
+
+public class SumaLiczbParzystychTablicy {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 1, 9, -4};
+        calculateEvenSum(numbers);
+    }
+    public static void calculateEvenSum(int[] numbers) {
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                sum +=numbers[i];
+            }
+        }
+        System.out.println("Suma liczb parzystych tablicy wynosi: " + sum);
+    }
+}
