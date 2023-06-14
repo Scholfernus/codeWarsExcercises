a8f82423bf4f9e409bcf401dc7e877a61abbf139
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-14 22:33:30 +0200

    Znajdowanie indexu szukanej liczby

---
+++ src/main/java/org/example/chatGPTzadania/Methods/FindFirstNumberIndex.java
@@ -0,0 +1,18 @@
+package org.example.chatGPTzadania.Methods;
+
+public class FindFirstNumberIndex {
+    public static void main(String[] args) {
+        int[] numbers = {5, 2, 8, 1, 9, -4};
+        int target = 7;
+        int index = findFirstIndex(numbers, target);
+        System.out.println("Indeks pierwszego wystąpienia liczby " + target + " to: " + index);
+    }
+    public static int findFirstIndex(int[] numbers, int target){
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i]==target){
+               return i;
+            }
+        }
+        return -1;
+    }
+}
