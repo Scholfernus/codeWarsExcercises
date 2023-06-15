f6a3ca7dc0fbf0cdefd397f815476a2e9883819c
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-15 18:01:15 +0200

    Scanner metoda

---
+++ src/main/java/org/example/chatGPTzadania/Methods/SelfControl.java
@@ -0,0 +1,19 @@
+package org.example.chatGPTzadania.Methods;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class SelfControl {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> numbers = new ArrayList<>();
+        System.out.println("Podaj dowolne liczby całkowite: ");
+        while (scanner.hasNextInt()){
+            int number = scanner.nextInt();
+            numbers.add(number);
+        }
+        System.out.println(numbers);
+    }
+}
