b370a0530e4aec4baff8278cbe2aa55d0766d3ca
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-25 23:15:34 +0200

    Arrrays ReverseWords ćwiczenie i kilka innych rozwiązań

---
+++ src/main/java/org/example/CodeWars/ReverseWords.java
@@ -0,0 +1,37 @@
+package org.example.CodeWars;
+
+import java.util.Arrays;
+
+public class ReverseWords {
+    public static void main(String[] args) {
+        System.out.println(reverseWords("The greatest victory is that which requires no battle"));
+    }
+    public static String reverseWords(String str) {
+        String[] split = str.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = split.length - 1; i >= 0; i--) {
+            sb.append(split[i]);
+            if (i > 0) {
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
+// List Words = Arrays.asList(str.split(" "));
+//     Collections.reverse(Words);
+//     return String.join(" ", Words);
+
+//List<String> words = Arrays.asList(str.split(" "));
+//    Collections.reverse(words);
+//    return String.join(" ", words);
+
+//return Arrays.stream(str.split(" ")).reduce((x, y) -> y+" "+x).get();
+
+//String[] s = str.split(" ");
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = s.length-1; i >=0; i--) {
+//            sb.append(s[i] + " ");
+//        }
+//        return sb.toString().trim();
+//    }
