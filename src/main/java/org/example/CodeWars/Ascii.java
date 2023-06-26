615bf92b72db9d684fccb8473ab2cb2ab889b94f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-26 23:10:05 +0200

    Ascii ćwiczenia z int na char

---
+++ src/main/java/org/example/CodeWars/Ascii.java
@@ -0,0 +1,13 @@
+package org.example.CodeWars;
+
+public class Ascii {
+    public static void main(String[] args) {
+        System.out.println(getChar(64));
+    }
+    public static char getChar(int c) {
+        char car = (char)c;
+        return car;
+    }
+}
+
+// return (char)c;
