0b0e0f4a53b26420301b9cd297d8fc590e4e5814
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-06-28 22:54:25 +0200

    Average school

---
+++ src/main/java/org/example/CodeWars/SchoolAverage.java
@@ -0,0 +1,20 @@
+package org.example.CodeWars;
+
+public class SchoolAverage {
+    public static void main(String[] args) {
+        int[] avr = new int[]{1, 2, 3, 4, 5};
+        System.out.println(getAverage(avr));
+    }
+
+    public static int getAverage(int[] marks) {
+        int average = 0;
+        for (int i = 0; i < marks.length; i++) {
+            average +=marks[i];
+        }
+        return average/ marks.length;
+    }
+}
+
+// return Arrays.stream(marks).sum() / marks.length;
+
+//
