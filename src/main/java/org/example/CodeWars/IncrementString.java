1cacffa3db2f62f0c0691640042f4407b53e8d68
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-03 22:57:04 +0200

    Dodawanie do Stringa cyfry o 1 większej

---
+++ src/main/java/org/example/CodeWars/IncrementString.java
@@ -0,0 +1,20 @@
+package org.example.CodeWars;
+
+public class IncrementString {
+    public static void main(String[] args) {
+        System.out.println(incrementString("foobar002"));
+    }
+    public static String incrementString(String str) {
+            char numb = 1;
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (i ==chars.length-1){
+                chars[i]+=numb;
+            }else break;
+            str = chars.toString();
+        }
+        return str;
+    }
+}
+
+// ten kod jest nieprawidłowy, nie działa poprawnie
