41ba42941b223ad01bb6be97791dabb9372a504c
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-06 20:01:35 +0200

    Kod zaczęty nie jest jeszcze rozwiązany

---
+++ src/main/java/org/example/CodeWars/QueensAndKingsStableMarriages.java
@@ -0,0 +1,14 @@
+package org.example.CodeWars;
+
+public class QueensAndKingsStableMarriages {
+    public static int[] bestMatch(int[][] queens, int[][] kings) {
+        int n = queens.length;
+        int[] match = new int[n];
+        // Do your magic
+        return match;
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
