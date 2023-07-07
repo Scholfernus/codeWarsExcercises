7b19a614fe6576639594dbf244ea62ee0cbba0b3
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-07 22:27:57 +0200

    Naprawa niekończącej się pętli, dodanie jednego warunku

---
+++ src/main/java/org/example/CodeWars/UnfinishedLoopHelp.java
@@ -0,0 +1,18 @@
+package org.example.CodeWars;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UnfinishedLoopHelp {
+    public static List CreateList(int number)
+    {
+        List list = new ArrayList();
+
+        for(int count = 1; count <= number; count++)
+        {
+            list.add(count);
+        }
+
+        return list;
+    }
+}
