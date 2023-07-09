4c14811771d711b9105a4ea91b22081874405219
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-09 22:55:50 +0200

    Wymęczony kod z drukowaniem 2 tablic, według zaleceń ćwiczenia prze printf %d

--- src/main/java/org/example/ZadanieSDA/TwoArrayExercise.java
+++ src/main/java/org/example/ZadanieSDA/TwoArrayExercise.java
@@ -6,14 +6,20 @@ public class TwoArrayExercise {
     public static void main(String[] args) {
         int[][] numbers = new int[][]{{0, 1, 2}, {3, 4, 5}};
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = 0; j < numbers.length; j++) {
-                for (var n : numbers) {
-                    System.out.printf("numbers[%d][%d] = %d ", i, j, i);
-                }
-                System.out.println();
+            for (int j = 0; j < numbers[i].length; j++) {
+                System.out.print(numbers[i][j] + " ");
             }
+            System.out.println();
         }
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers[i].length; j++) {
+                System.out.printf("numbers[%d][%d] = %d \n", i, j, numbers[i][j]);
+            }
+            System.out.println();
+        }
+
         System.out.println(Arrays.toString(numbers));
     }
 }
+
 
