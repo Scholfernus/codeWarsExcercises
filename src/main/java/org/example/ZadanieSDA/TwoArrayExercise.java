c7bf20ad5dae4d308326d0acbf3a4c7d4d8e27b8
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-09 10:00:10 +0200

    Zmiana klasy - zadanie nie jest skończone

--- src/main/java/org/example/ZadanieSDA/TwoArrayExercise.java
+++ src/main/java/org/example/ZadanieSDA/TwoArrayExercise.java
@@ -2,7 +2,7 @@ package org.example.ZadanieSDA;
 
 import java.util.Arrays;
 
-public class ZadanieSDA {
+public class TwoArrayExercise {
     public static void main(String[] args) {
         int[][] numbers = new int[][]{{0, 1, 2}, {3, 4, 5}};
         for (int i = 0; i < numbers.length; i++) {
