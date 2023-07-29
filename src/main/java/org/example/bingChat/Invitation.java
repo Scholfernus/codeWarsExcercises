71751ea02718d3e8d51d916d091e34ff8cb72c94
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-29 17:38:14 +0200

    Bardzo proste zadanie

---
+++ src/main/java/org/example/bingChat/Invitation.java
@@ -0,0 +1,9 @@
+package org.example.bingChat;
+
+public class Invitation {
+    public static void main(String[] args) {
+        String name = "Paul";
+        System.out.println("Czesc " + name );
+    }
+
+}
