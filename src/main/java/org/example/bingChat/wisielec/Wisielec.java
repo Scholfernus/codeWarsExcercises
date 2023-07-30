610824294eec9bb5d5574fb834ac205891aacd32
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-30 17:55:26 +0200

    Poprawiona wersja kodu - kompiluje się, ale są jeszcze luki.

--- src/main/java/org/example/bingChat/wisielec/Wisielec.java
+++ src/main/java/org/example/bingChat/wisielec/Wisielec.java
@@ -6,24 +6,35 @@ import java.util.Scanner;
 
 public class Wisielec {
     public static void main(String[] args) {
+        StringBuilder falseLetter = new StringBuilder("Błędne litery: ");
         List<String> words = List.of("java", "python", "csharp", "c++", "script");
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         String word;
         System.out.println(words);
-        char[] userWords = new char[]{};
+        word = words.get(random.nextInt(words.size()));
+        char[] userWords = new char[word.length()];
 
-        while (!gameOver()) {
-            word = words.get(random.nextInt(words.size()));
+        while (gameOver()) {
             for (int i = 0; i < word.length(); i++) {
                 userWords[i] = '-';
             }
-            System.out.print("Podaj dowolną literę:");
         }
+        System.out.println(userWords);
+        System.out.print("Podaj dowolną literę:");
+        char s = scanner.next().charAt(0);
+        for (int i = 0; i < userWords.length; i++) {
+            if (word.contains(String.valueOf(s))) {
+                userWords[i] = s;
+            } else {
+                falseLetter.append(" s");
+            }
+        }
+
     }
 
     private static boolean gameOver() {
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
         return false;
     }
 }
