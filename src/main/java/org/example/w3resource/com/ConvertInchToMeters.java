69dafa3fd2f4f7203fbb42a2afe5b536695f90d1
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-31 21:28:40 +0200

    Zamiana cali na metry

---
+++ src/main/java/org/example/w3resource/com/ConvertInchToMeters.java
@@ -0,0 +1,14 @@
+package org.example.w3resource.com;
+
+import java.util.Scanner;
+
+public class ConvertInchToMeters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj długość w calach: ");
+        long cal = scanner.nextLong();
+        double metr = 0.0254;
+        double meters = (double) cal * metr;
+        System.out.println(meters);
+    }
+}
