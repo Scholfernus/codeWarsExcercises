3f7ca70891ddb61c55f579dc2c09afa1d04e4ab2
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-31 22:30:11 +0200

    Wyświetlanie ilości lat i dni na podstawie minut

---
+++ src/main/java/org/example/w3resource/com/ConvertMinutesToYearAndDays.java
@@ -0,0 +1,15 @@
+package org.example.w3resource.com;
+
+import java.util.Scanner;
+
+public class ConvertMinutesToYearAndDays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę minut: ");
+        long minutes = scanner.nextLong();
+        long hours = minutes / 60;
+        long days = (hours / 24) % 365;
+        int years = (int) (minutes/(60*24*365));
+        System.out.println(minutes + " minut, to " + years + " lat i " + days + " dni.");
+    }
+}
