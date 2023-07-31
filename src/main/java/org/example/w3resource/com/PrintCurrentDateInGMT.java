897af9f8dbffd059f38677eb391308b30518ab4d
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-31 22:09:02 +0200

    Wyświetlanie aktualnego czasu i daty - wykorzystanie klas LocalDate etc...

---
+++ src/main/java/org/example/w3resource/com/PrintCurrentDateInGMT.java
@@ -0,0 +1,18 @@
+package org.example.w3resource.com;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class PrintCurrentDateInGMT {
+    public static void main(String[] args) {
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+        String format = gmtTime.format(formatter);
+        System.out.println("Aktualny czas GMT "+ format);
+        LocalDate currentDate = LocalDate.now();
+        System.out.println(currentDate);
+    }
+}
+
