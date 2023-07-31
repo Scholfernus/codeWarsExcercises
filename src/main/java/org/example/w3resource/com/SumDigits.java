2510dd08b8569ab8128b17e805c66914c5e4fc17
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-31 21:31:53 +0200

    Suma cyfr danej liczby - komentarz do innego rozwiązania

--- src/main/java/org/example/w3resource/com/SumDigits.java
+++ src/main/java/org/example/w3resource/com/SumDigits.java
@@ -16,3 +16,13 @@ public class SumDigits {
         System.out.println("Suma liczby " + number + " wynosi " + sum);
     }
 }
+
+//        int firstDigit = number % 10;
+//        int remainingNumber = number / 10;
+//        int SecondDigit = remainingNumber % 10;
+//        remainingNumber = remainingNumber / 10;
+//        int thirdDigit = remainingNumber % 10;
+//        remainingNumber = remainingNumber / 10;
+//        int fourthDigit = remainingNumber % 10;
+//        int sum = thirdDigit + SecondDigit + firstDigit + fourthDigit;
+//        System.out.println("The sum of all digits in " + num + " is " + sum);
