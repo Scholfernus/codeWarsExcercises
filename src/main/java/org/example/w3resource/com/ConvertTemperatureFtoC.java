4260c7aaf7cc2cd400588306b8b7ffa1102fb95f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-31 21:29:16 +0200

    Zamiana Fahrenheita na Celsjusza

---
+++ src/main/java/org/example/w3resource/com/ConvertTemperatureFtoC.java
@@ -0,0 +1,20 @@
+package org.example.w3resource.com;
+
+import java.util.Scanner;
+
+public class ConvertTemperatureFtoC {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj temperaturę w stopniach Fahreheita:");
+        double fahrenheit = scanner.nextDouble();
+
+        double tempInCelsius = temp(fahrenheit);
+        System.out.printf("Dla temperatury %.2f stopni Fahrenheita, temperatura wyrażona\nw stopniach Celsjusza wynosi %.2f", fahrenheit, tempInCelsius);
+    }
+
+    private static double temp(double fahrenheit) {
+        double temp = 1;
+        return (fahrenheit - 32) * 5 / 9;
+    }
+}
