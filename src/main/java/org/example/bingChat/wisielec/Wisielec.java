209bd16e1cc7bb3643f1d6122a5a3852e3ac72f3
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-31 19:18:00 +0200

    Wersja ostateczna. Wszystko się kompiluje i zwraca poprawny wynik.

--- src/main/java/org/example/bingChat/wisielec/Wisielec.java
+++ src/main/java/org/example/bingChat/wisielec/Wisielec.java
@@ -6,35 +6,75 @@ import java.util.Scanner;
 
 public class Wisielec {
     public static void main(String[] args) {
-        StringBuilder falseLetter = new StringBuilder("Błędne litery: ");
+        StringBuilder falseLetters = new StringBuilder("Błędne litery: ");
         List<String> words = List.of("java", "python", "csharp", "c++", "script");
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         String word;
         System.out.println(words);
         word = words.get(random.nextInt(words.size()));
-        char[] userWords = new char[word.length()];
-
-        while (gameOver()) {
-            for (int i = 0; i < word.length(); i++) {
-                userWords[i] = '-';
-            }
+        char[] userWord = new char[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            userWord[i] = '-';
         }
-        System.out.println(userWords);
-        System.out.print("Podaj dowolną literę:");
-        char s = scanner.next().charAt(0);
-        for (int i = 0; i < userWords.length; i++) {
-            if (word.contains(String.valueOf(s))) {
-                userWords[i] = s;
+        while (!gameOver(word, userWord)) {
+            System.out.println(userWord);
+            System.out.print("Podaj dowolną literę: ");
+            char s = scanner.nextLine().toLowerCase().charAt(0); // Pobranie pierwszego znaku (litery)
+            if (containsLetter(String.valueOf(userWord), s)) {
+                System.out.println("Ta litera już została podana. Spróbuj inną.");
             } else {
-                falseLetter.append(" s");
+                boolean found = false;
+                for (int i = 0; i < word.length(); i++) {
+                    if (word.charAt(i) == s) {
+                        userWord[i] = s;
+                        found = true;
+                    }
+                }
+
+                if (!found) {
+                    System.out.println(falseLetters.append(s).append(", "));
+                }
             }
         }
-
     }
 
-    private static boolean gameOver() {
-//        System.out.println("Hello World");
+    private static boolean containsLetter(String str, char letter) {
+        for (char c : str.toCharArray()) {
+            if (c == letter) {
+                return true;
+            }
+        }
         return false;
+    }
+    private static boolean gameOver(String word, char[] userWord) {
+        if (isCorrect(userWord)) {
+            System.out.println("Gratulacje! Odgadłeś słowo !!!");
+            System.out.println("To słowo to: " + word);
+            return true;
+        } else if (isLoose(userWord)) {
+            System.out.println("Niestety, przegrałeś. Szukane słowo to: " + word);
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isCorrect(char[] userWord) {
+         for (char c : userWord) {
+            if (c == '-') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isLoose(char[] userWord) {
+        int numberOfIncorrectWords = 0;
+        for (char c : userWord) {
+            if (c == '-') {
+                numberOfIncorrectWords++;
+            }
+        }
+        return numberOfIncorrectWords >= 12;
     }
 }
