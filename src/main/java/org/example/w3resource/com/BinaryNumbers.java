00bb6f642504432c053ee69055e7da2b5641c9da
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-01 12:46:27 +0200

    Sum of binary numbers

---
+++ src/main/java/org/example/w3resource/com/BinaryNumbers.java
@@ -0,0 +1,10 @@
+package org.example.w3resource.com;
+
+public class BinaryNumbers {
+    public static void main(String[] args) {
+        int number = 10;
+        int number2 = 11;
+        String binaryString = Integer.toBinaryString(number + number2);
+        System.out.println(binaryString);
+    }
+}
