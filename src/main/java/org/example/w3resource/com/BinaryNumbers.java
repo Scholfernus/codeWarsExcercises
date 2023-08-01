168aca777381ca579fa93b4c500743ef996b03b3
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-01 13:05:58 +0200

    Sum of binary numbers - poprawa kodu

--- src/main/java/org/example/w3resource/com/BinaryNumbers.java
+++ src/main/java/org/example/w3resource/com/BinaryNumbers.java
@@ -1,10 +1,16 @@
 package org.example.w3resource.com;
 
+import java.util.Scanner;
+
 public class BinaryNumbers {
     public static void main(String[] args) {
-        int number = 10;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Give a number:");
+        int number = input.nextInt();
         int number2 = 11;
-        String binaryString = Integer.toBinaryString(number + number2);
+        String binaryString = Integer.toBinaryString(number);
+        String binaryString2 = Integer.toBinaryString(number2);
         System.out.println(binaryString);
+        System.out.println(binaryString2);
     }
 }
