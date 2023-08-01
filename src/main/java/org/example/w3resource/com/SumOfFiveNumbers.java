9ca1c8d96f0c1e4b0ada1bc2a79ff7c1e48fee70
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-01 18:17:39 +0200

    Pętla for - sma 5-ciu dowolnych liczb

---
+++ src/main/java/org/example/w3resource/com/SumOfFiveNumbers.java
@@ -0,0 +1,19 @@
+package org.example.w3resource.com;
+
+import java.util.Scanner;
+
+public class SumOfFiveNumbers {
+    public static void main(String[] args) {
+        int sum = 0;
+        double average;
+        System.out.println("Input five numbers: ");
+        Scanner scan = new Scanner(System.in);
+
+        for (int i = 0; i < 5; i++) {
+            int num = scan.nextInt();
+            sum += num;
+        }
+        average = (double) sum / 5;
+        System.out.println("The sum of the 5 numbers is " + sum + "\nThe average is " + average);
+    }
+}
