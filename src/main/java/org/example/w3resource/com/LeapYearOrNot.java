221fe344aa10e80095e8538f653ee53f51c718fb
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-01 18:17:08 +0200

    If - czy rok jest przestępny

---
+++ src/main/java/org/example/w3resource/com/LeapYearOrNot.java
@@ -0,0 +1,23 @@
+package org.example.w3resource.com;
+
+import java.util.Scanner;
+
+public class LeapYearOrNot {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a year: ");
+        int year = scanner.nextInt();
+        if ((year % 400 == 0) || (year % 4 == 0) && (year % 100 != 0)) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+    }
+}
+
+// Write a Java program that takes a year from the user and prints whether it is a leap year or not.
+//
+//Test Data
+//Input the year: 2016
+//Expected Output :
+//2016 is a leap year
