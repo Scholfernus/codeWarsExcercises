639a32b867dc4009afa7682ff71043eea6398747
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-01 18:18:32 +0200

    Wyświetl 10 pierwszych liczb

---
+++ src/main/java/org/example/w3resource/com/WriteFirst10NaturalNumbers.java
@@ -0,0 +1,29 @@
+package org.example.w3resource.com;
+
+import java.util.Arrays;
+
+public class WriteFirst10NaturalNumbers {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
+
+// Write a Java program to display the first 10 natural numbers.
+//
+//Expected Output :
+//
+//The first 10 natural numbers are:
+//
+//1
+//2
+//3
+//4
+//5
+//6
+//7
+//8
+//9
+//10
