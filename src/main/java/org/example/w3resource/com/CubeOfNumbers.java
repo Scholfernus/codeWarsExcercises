81b986b9f2236ecc5b249226c40f5c2cbb7b13d2
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-02 22:47:27 +0200

    Cube of some 4 numbers

---
+++ src/main/java/org/example/w3resource/com/CubeOfNumbers.java
@@ -0,0 +1,27 @@
+package org.example.w3resource.com;
+
+import java.util.Scanner;
+
+public class CubeOfNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Give some 4 numbers: ");
+        int num;
+        num = scanner.nextInt();
+        for (int i = 1; i <= num; i++) {
+            System.out.println("Number is " + i +
+                    " and cube of " + i + " is : " + (i * i * i));
+        }
+    }
+}
+
+//Write a Java program to display the cube of the given number up to an integer.
+//
+//Test Data
+//Input number of terms : 4
+//Expected Output :
+//
+//Number is : 1 and cube of 1 is : 1
+//Number is : 2 and cube of 2 is : 8
+//Number is : 3 and cube of 3 is : 27
+//Number is : 4 and cube of 4 is : 64
