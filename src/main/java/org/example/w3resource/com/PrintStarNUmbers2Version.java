0666a9545e6deab415f7946358d4fc015a73e3b7
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-03 22:31:52 +0200

    Efekt gwiazdy - wyświetl na  ekranie 2-ga modyfikacja

---
+++ src/main/java/org/example/w3resource/com/PrintStarNUmbers2Version.java
@@ -0,0 +1,21 @@
+package org.example.w3resource.com;
+
+public class PrintStarNUmbers2Version {
+    public static void main(String[] args) {
+        for (int i = 1; i <=4; i++) {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+}
+
+// Write a program in Java to make such a pattern like a right angle triangle with a number which repeats a number in a row.
+//
+//The pattern is as follows :
+//
+//1
+//22
+//333
+//4444
