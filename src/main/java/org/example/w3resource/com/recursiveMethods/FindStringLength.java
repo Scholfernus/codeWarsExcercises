79946bea73554c806b36c0a7f376e0677180d3af
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-09 21:40:25 +0200

    Znajdź długość zdania - ilość znaków

---
+++ src/main/java/org/example/w3resource/com/recursiveMethods/FindStringLength.java
@@ -0,0 +1,35 @@
+package org.example.w3resource.com.recursiveMethods;
+
+public class FindStringLength {
+    public static int wordLength(String str) {
+        if (str.isEmpty()) {
+            return 0;
+        }
+        return str.length();
+    }
+    public static void main(String[] args) {
+        String word = "Find a string length";
+        System.out.printf("Długość słowa ma %d znaków", wordLength(word));
+    }
+}
+
+  class StringLengthCalculator {
+
+  public static int calculateStringLength(String str) {
+    // Base case: if the string is empty, the length is 0
+    if (str.isEmpty()) {
+      return 0;
+    }
+
+    // Recursive case: remove the first character of the
+    // string and recursively call the method
+    // with the remaining substring, and add 1 to the length
+    return 1 + calculateStringLength(str.substring(1));
+  }
+
+  public static void main(String[] args) {
+    String input = "Java Exercises!";
+    int length = calculateStringLength(input);
+    System.out.println("The length of the string \"" + input + "\" is: " + length);
+  }
+}
