8b3587e652866b91ca3ca1e1b123649180debfb5
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-10 05:33:58 +0200

    Enum - kształty figur.

---
+++ src/main/java/org/example/w3resource/com/enumTypes/Shapes.java
@@ -0,0 +1,24 @@
+package org.example.w3resource.com.enumTypes;
+
+public class Shapes {
+    public static enum Shape {
+        SQUARE,
+        TRIANGLE,
+        CIRCLE,
+        RECTANGLE,
+        HEXAGON
+    }
+
+    public static void main(String[] args) {
+        Shape firstElement = Shape.SQUARE;
+        Shape secondElement = Shape.TRIANGLE;
+        Shape thirdElement = Shape.CIRCLE;
+        Shape fourthElement = Shape.RECTANGLE;
+        Shape fifthElement = Shape.HEXAGON;
+        System.out.println("firstElement is " + firstElement
+                + ".\nsecondElement is " + secondElement
+                + ".\nthirdElement is " + thirdElement
+                + ".\nfourthElement is " + fourthElement
+                + ".\nfifthElement is " + fifthElement + ".");
+    }
+}
