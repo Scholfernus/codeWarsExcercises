aba9986f1259a3ae443516ed91fe81cc66cd85cf
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-18 22:47:47 +0200

    Shape3D - klasa abstrakcyjna, do dokończenia www.w3resource.com

---
+++ src/main/java/org/example/w3resource/com/abstractClasses/Shape3D.java
@@ -0,0 +1,15 @@
+package org.example.w3resource.com.abstractClasses;
+
+abstract class Shape3D {
+    public abstract void calculateVolume();
+    public abstract void calculateSurfaceArea();
+    public static void main(String[] args) {
+
+    }
+}
+class Sphere extends Shape3D{
+
+}
+class Cube extends Shape3D{
+
+}
