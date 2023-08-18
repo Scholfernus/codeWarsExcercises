e0595bc96fbf7c33e49abfe7ef77beeae0170492
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-18 22:43:00 +0200

    Employee - klasa abstrakcyjna, do dokończenia www.w3resource.com

---
+++ src/main/java/org/example/w3resource/com/Employee.java
@@ -0,0 +1,11 @@
+package org.example.w3resource.com;
+abstract class Employee {
+public abstract void calculateSalary();
+
+}
+class Manager extends Employee{
+
+}
+class Programmer extends Employee{
+
+}
