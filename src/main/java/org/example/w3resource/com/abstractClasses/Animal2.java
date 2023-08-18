3b716901984959cbe1f3782ae7a1eb51a75f5984
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-18 22:38:41 +0200

    Animal2 - klasa abstrakcyjna, do dokończenia www.w3resource.com

---
+++ src/main/java/org/example/w3resource/com/abstractClasses/Animal2.java
@@ -0,0 +1,16 @@
+package org.example.w3resource.com.abstractClasses;
+
+public abstract class Animal2 {
+    abstract void eat();
+    abstract void sleep();
+
+}
+class Lion extends Animal2{
+
+}
+class  Tiger extends Animal2{
+
+}
+class Deer extends Animal2{
+
+}
