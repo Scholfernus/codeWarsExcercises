43274d02c0cd8ab54289c7045dc999c450aeb762
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-19 13:51:38 +0200

    Shape3D - klasa abstrakcyjna, gotowe www.w3resource.com

--- src/main/java/org/example/w3resource/com/abstractClasses/Shape3D.java
+++ src/main/java/org/example/w3resource/com/abstractClasses/Shape3D.java
@@ -1,15 +1,66 @@
 package org.example.w3resource.com.abstractClasses;
 
 abstract class Shape3D {
-    public abstract void calculateVolume();
-    public abstract void calculateSurfaceArea();
-    public static void main(String[] args) {
+    protected double sideA, sideB;
 
+    public Shape3D() {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public abstract double calculateVolume();
+
+    public abstract double calculateSurfaceArea();
+
+}
+
+class Sphere extends Shape3D {
+    private double sphereArea;
+    private double radius;
+
+    public Sphere(double radius) {
+        super();
+        this.sphereArea = sphereArea;
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }
+
+    @Override
+    public double calculateSurfaceArea() {
+
+        return 4 * Math.PI * (radius * radius);
     }
 }
-class Sphere extends Shape3D{
 
+class Cube extends Shape3D {
+    public Cube(double sideA, double sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    @Override
+    public double calculateVolume() {
+        return Math.pow(sideA, sideB);
+    }
+
+    @Override
+    public double calculateSurfaceArea() {
+        return 6 * (sideA * sideB);
+    }
 }
-class Cube extends Shape3D{
 
+class Main6 {
+    public static void main(String[] args) {
+        Shape3D sphere = new Sphere(7.0);
+        Shape3D cube = new Cube(4.0, 6.0);
+        System.out.printf("Sphere Volume: %.2f\n", sphere.calculateVolume());
+        System.out.printf("Sphere Surface Area: %.2f",sphere.calculateSurfaceArea());
+
+        System.out.printf("\nCube Volume: %.2f\n",cube.calculateVolume());
+        System.out.printf("Cube Surface Area: %.2f",cube.calculateSurfaceArea());
+    }
 }
