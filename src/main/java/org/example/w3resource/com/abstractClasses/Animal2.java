b77558b0b87f391aaef7e02be36dcad001660d4e
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-19 13:49:24 +0200

    Animal2 - klasa abstrakcyjna, gotowe www.w3resource.com

--- src/main/java/org/example/w3resource/com/abstractClasses/Animal2.java
+++ src/main/java/org/example/w3resource/com/abstractClasses/Animal2.java
@@ -1,16 +1,59 @@
 package org.example.w3resource.com.abstractClasses;
 
-public abstract class Animal2 {
-    abstract void eat();
-    abstract void sleep();
+abstract class Animal2 {
+    public abstract void eat();
 
+    public abstract void sleep();
 }
-class Lion extends Animal2{
 
+class Lion2 extends Animal2 {
+    @Override
+    public void eat() {
+        System.out.println("Lion eats meat.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Lion sleeps on grassland.");
+    }
 }
-class  Tiger extends Animal2{
 
+class Tiger2 extends Animal2 {
+    @Override
+    public void eat() {
+        System.out.println("Tiger eats meat and occasionally hunts in water.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Tiger sleeps in a hidden spot.");
+    }
 }
-class Deer extends Animal2{
 
+class Deer extends Animal2 {
+    @Override
+    public void eat() {
+        System.out.println("Deer grazes on grass and leaves.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Deer sleeps in open fields.");
+    }
+}
+class Main4{
+    public static void main(String[] args) {
+        Lion2 lion2 = new Lion2();
+        lion2.eat();
+        lion2.sleep();
+        System.out.println();
+        Tiger2 tiger2 = new Tiger2();
+        tiger2.eat();
+        tiger2.sleep();
+        System.out.println();
+        Deer deer = new Deer();
+        deer.eat();
+        deer.sleep();
+
+    }
 }
