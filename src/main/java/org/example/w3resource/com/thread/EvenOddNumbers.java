a0f3261fda7037a3e1933fb15ce6cd6a5ea031e4
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-20 15:13:51 +0200

    Thread - wątek - parzyste - nieparzyste www.w3resource.com

---
+++ src/main/java/org/example/w3resource/com/thread/EvenOddNumbers.java
@@ -0,0 +1,46 @@
+package org.example.w3resource.com.thread;
+
+public class EvenOddNumbers {
+    private static final int MAX_NUMBER = 20;
+    private static final Object lock = new Object();
+    private static boolean isEvenTurn = true;
+
+    public static void main(String[] args) {
+        Thread evenThread = new Thread(() -> {
+            for (int i = 2; i <= MAX_NUMBER; i += 2) {
+                synchronized (lock) {
+                    while (!isEvenTurn) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Even Number from evenThread: " + i);
+                    isEvenTurn = false;
+                    lock.notify();
+                }
+            }
+        });
+
+        Thread oddThread = new Thread(() -> {
+            for (int i = 1; i < MAX_NUMBER; i += 2) {
+                synchronized (lock) {
+                    while (isEvenTurn) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Odd number from oddThread: " + i);
+                    isEvenTurn = true;
+                    lock.notify();
+                }
+            }
+        });
+        evenThread.start();
+        oddThread.start();
+    }
+}
+
