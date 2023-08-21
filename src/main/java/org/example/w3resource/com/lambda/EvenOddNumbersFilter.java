37d4b36f995012004bc4a5d9de13ea325c6d373c
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-21 19:16:47 +0200

    Lambda - Parzyste i nieparzyste, poprawiony kod, ale wymaga delikatnej poprawki
    www.w3resource.com

--- src/main/java/org/example/w3resource/com/lambda/EvenOddNumbersFilter.java
+++ src/main/java/org/example/w3resource/com/lambda/EvenOddNumbersFilter.java
@@ -12,21 +12,23 @@ public class EvenOddNumbersFilter {
         int[] numbers = {1, 3, 5, 2, 8, 6, 4, 9, 3, 1, 5, 45, 78, 11, 0, -4, -5};
         System.out.println(Arrays.toString(numbers));
         List<Integer> list = Arrays.asList(11, 23, 98, 34, 15, 32, 42, 80, 99, 100);
-        List nums = List.of(numbers);
         for (var n : list) {
             System.out.print(n + " ");
         }
-        System.out.println();
+        System.out.println("\nLiczby parzyste.");
         List<Integer> evenNumbers = list.stream().filter(n -> n % 2 == 0).collect(Collectors.toList());
         Supplier<IntStream> intStreamSupplier = () -> Arrays.stream(numbers).filter(i -> i % 2 == 0);
-        for (var n: evenNumbers) {
-            System.out.print(n + ", ");
-        }
+        IntStream evenStream = intStreamSupplier.get();
+        int[] evenArray = evenStream.toArray();
+
+        System.out.println("Liczby parzyste: " + Arrays.toString(evenArray));
+        System.out.println("\nLiczby nieparzyste.");
         List<Integer> oddNumbers = list.stream().filter(n -> n % 2 != 0).collect(Collectors.toList());
-        for (var n: oddNumbers) {
+        for (var n : oddNumbers) {
             System.out.print(n + " ");
         }
-        System.out.println("Liczby parzyste: " + intStreamSupplier.toString());
+        System.out.println();
+        System.out.println("Liczby parzyste: " + intStreamSupplier);
         Supplier<IntStream> streamSupplier = () -> Arrays.stream(numbers).filter(i -> i % 2 != 0);
         System.out.println("Liczby nieparzyste: " + streamSupplier);
     }
