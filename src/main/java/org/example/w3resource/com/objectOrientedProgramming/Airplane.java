428e3a2f2e95a8b20b9e4f59d968db0e874dc8be
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-30 20:38:04 +0200

    Object Oriented Programming -Airplane - z wykorzystaniem LocalTime www.w3resource.com

--- src/main/java/org/example/w3resource/com/objectOrientedProgramming/Airplane.java
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/Airplane.java
@@ -51,7 +51,10 @@ public class Airplane {
     public void checkStatus() {
         if (delayTime == 0) {
             System.out.println("Flight " + flightNumber + " is on time.");
-        } else {
+        }else if (delayTime>300){
+            System.out.println("Flight " + flightNumber + " is canceled.");
+        }
+        else {
             System.out.println("Flight " + flightNumber + " is delayed " + delayTime + " minutes.");
         }
     }
@@ -62,17 +65,22 @@ class Main11 {
         Airplane flight1 = new Airplane("CDE345", "London", LocalTime.of(10, 30));
         Airplane flight2 = new Airplane("KUI765", "New York", LocalTime.of(14, 0));
         Airplane flight3 = new Airplane("JUY456", "Paris", LocalTime.of(13, 0));
+        Airplane flight4 = new Airplane("LOT326", "Warsaw", LocalTime.of(15, 33));
         System.out.println("Flight status: ");
         flight1.checkStatus();
         flight2.checkStatus();
         flight3.checkStatus();
+        flight4.checkStatus();
 
         flight1.delay(40);
         flight2.delay(110);
+        flight3.delay(299);
+        flight4.delay(301);
 
         System.out.println("\nCurrent flight status:");
         flight1.checkStatus();
         flight2.checkStatus();
         flight3.checkStatus();
+        flight4.checkStatus();
     }
 }
