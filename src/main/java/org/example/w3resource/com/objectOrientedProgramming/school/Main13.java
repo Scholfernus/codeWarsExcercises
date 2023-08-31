d3472331fc90b7132ac0cf6ef54ae606241b31c6
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-08-31 22:23:35 +0200

    Object Oriented Programming - Main - klasa z pakietu School

---
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/school/Main13.java
@@ -0,0 +1,79 @@
+package org.example.w3resource.com.objectOrientedProgramming.school;
+
+public class Main13 {
+    public static void main(String[] args) {
+        School school = new School();
+        Student student1 = new Student("Ricky", 15);
+        Student student2 = new Student("Karolina", 16);
+        Student student3 = new Student("Felicia", 15);
+        Student student4 = new Student("Norbert", 16);
+
+
+        school.addStudent(student1);
+        school.addStudent(student2);
+        school.addStudent(student3);
+        school.addStudent(student4);
+
+        Teacher teacher1 = new Teacher("Amelia", "Math");
+        Teacher teacher2 = new Teacher("Elise", "English");
+        Teacher teacher3 = new Teacher("Angelika", " Science");
+
+        school.addTeacher(teacher1);
+        school.addTeacher(teacher2);
+        school.addTeacher(teacher3);
+
+        SchoolClass mathClass = new SchoolClass("Mathematics", teacher1);
+        mathClass.addStudent(student1);
+        mathClass.addStudent(student2);
+        mathClass.addStudent(student3);
+        mathClass.addStudent(student4);
+
+        SchoolClass englishClass = new SchoolClass("English", teacher2);
+        englishClass.addStudent(student1);
+        englishClass.addStudent(student2);
+        englishClass.addStudent(student3);
+
+        SchoolClass scienceClass = new SchoolClass("Science", teacher3);
+        scienceClass.addStudent(student1);
+        scienceClass.addStudent(student2);
+        scienceClass.addStudent(student3);
+        scienceClass.addStudent(student4);
+
+        school.addClass(mathClass);
+        school.addClass(englishClass);
+        school.addClass(scienceClass);
+
+        System.out.println("School information:");
+        System.out.println("Number of students: " + school.getStudents().size());
+        System.out.println("Number of teachers: " + school.getTeachers().size());
+        System.out.println("Number of classes: " + school.getClasses().size());
+        System.out.println();
+
+        System.out.println("Math class information:");
+        System.out.println("Class name: " + mathClass.getClassName());
+        System.out.println("Teacher: " + mathClass.getTeacher().getName());
+        System.out.println("Number of students: " + mathClass.getStudents().size());
+        System.out.println();
+
+        System.out.println("English class information:");
+        System.out.println("Class name: " + englishClass.getClassName());
+        System.out.println("Teacher: " + englishClass.getTeacher().getName());
+        System.out.println("Number of students: " + englishClass.getStudents().size());
+        System.out.println();
+
+        System.out.println("Science class information:");
+        System.out.println("Class name: " + scienceClass.getClassName());
+        System.out.println("Teacher: " + scienceClass.getTeacher().getName());
+        System.out.println("Number of students: " + scienceClass.getStudents().size());
+        System.out.println();
+
+        school.removeStudent(student1);
+        school.removeTeacher(teacher2);
+        school.removeClass(mathClass);
+
+        System.out.println("School information after removing one student, teacher and Class:");
+        System.out.println("Number of students: " + school.getStudents().size());
+        System.out.println("Number of teachers: " + school.getTeachers().size());
+        System.out.println("Number of classes: " + school.getClasses().size());
+    }
+}
