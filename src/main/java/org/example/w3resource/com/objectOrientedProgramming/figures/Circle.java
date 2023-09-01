37bac3d74d28957a56156b006b2ee47d627a1851
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-01 21:47:57 +0200

    Object Oriented Programming - Circle - klasa rozszerzająca Shape

---
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/figures/Circle.java
@@ -0,0 +1,19 @@
+package org.example.w3resource.com.objectOrientedProgramming.figures;
+
+public class Circle extends Shape {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
