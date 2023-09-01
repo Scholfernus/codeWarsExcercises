326b0d54054f6ba8b264dbe7cd479fa28979a70d
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-01 21:46:20 +0200

    Object Oriented Programming - Shape - klasa bazowa

---
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/figures/Shape.java
@@ -0,0 +1,7 @@
+package org.example.w3resource.com.objectOrientedProgramming.figures;
+
+public abstract class Shape {
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+}
