cdcb1523ee0401d893e8318ee83034e285139914
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-01 21:47:22 +0200

    Object Oriented Programming - Rectangle - klasa rozszerzająca Shape

---
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/figures/Rectangle.java
@@ -0,0 +1,21 @@
+package org.example.w3resource.com.objectOrientedProgramming.figures;
+
+public class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double getArea() {
+        return length * width;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+}
