7342f7ab2bcbae459d6d2711358be890f628198b
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-04 22:01:57 +0200

    Object Oriented Programming - Main - klasa wywołująca klasę Restaurant.

---
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/restaurant/Main.java
@@ -0,0 +1,7 @@
+package org.example.w3resource.com.objectOrientedProgramming.restaurant;
+
+public class Main {
+    public static void main(String[] args) {
+        Restaurant restaurant = new Restaurant();
+    }
+}
