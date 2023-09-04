2d2394bdf85a7909bbdcfdf15154e5190e6a7a0a
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-04 21:48:03 +0200

    Object Oriented Programming - Movie - koniec kodu

--- src/main/java/org/example/w3resource/com/objectOrientedProgramming/Movie.java
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/Movie.java
@@ -1,6 +1,7 @@
 package org.example.w3resource.com.objectOrientedProgramming;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Movie {
     private String title;
@@ -8,16 +9,37 @@ public class Movie {
     private ArrayList<String> actors;
     private ArrayList<Review> reviews;
 
-    public Movie(String title, String director, ArrayList<String> actors, ArrayList<Review> reviews) {
+    public Movie(String title, String director, ArrayList<String> actors) {
         this.title = title;
         this.director = director;
         this.actors = actors;
-        this.reviews = reviews;
+        this.reviews = new ArrayList<Review>();
+    }
+
+    public void addReview(Review review) {
+        this.reviews.add(review);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public ArrayList<String> getActors() {
+        return actors;
+    }
+
+    public ArrayList<Review> getReviews() {
+        return reviews;
     }
 }
-class Review{
+
+class Review {
     private String reviewText;
-    private  String reviewerName;
+    private String reviewerName;
     private double rating;
 
     public Review(String reviewText, String reviewerName, double rating) {
@@ -36,5 +58,42 @@ class Review{
 
     public double getRating() {
         return rating;
+    }
+}
+
+class Main15 {
+    public static void main(String[] args) {
+        Movie movie1 = new Movie("Titanic", "James Cameron",
+                new ArrayList<String>(Arrays.asList("Leonardo DICaprio", "Kate Winslet")));
+
+        Review review1 = new Review("Great movie!", "Irvine Rolf", 4.5);
+        Review review2 = new Review("Highly recommended!", "Ashkii Karlheinz", 4.5);
+        Review review3 = new Review("A classic that never gets old.", "Nele Athol", 4.0);
+        Review review4 = new Review("Great movie!", "Cipactli Anselma", 4.0);
+        Review review5 = new Review("Highly recommended!", "Martin Nadine", 4.0);
+
+        movie1.addReview(review1);
+        movie1.addReview(review2);
+        movie1.addReview(review3);
+        movie1.addReview(review4);
+
+        Movie movie2 = new Movie("The Godfather", "Francis Ford Coppola",
+                new ArrayList<String>(Arrays.asList("Marlon Brando", "Al Pacino", "James Caan")));
+        System.out.println("\nReviews for '" + movie1.getTitle() + "':");
+        for (Review review : movie1.getReviews()) {
+            System.out.println(review.getReviewText() + " by " + review.getReviewerName() +
+                    " - " + review.getRating());
+        }
+        movie2.addReview(review1);
+        movie2.addReview(review4);
+        movie2.addReview(review5);
+
+        System.out.println("\nReviews for '" + movie2.getTitle() + "':");
+        for (Review review : movie2.getReviews()) {
+            System.out.println(review.getReviewText() + " by " + review.getReviewerName()
+                    + " - " + review.getRating());
+        }
+
+
     }
 }
