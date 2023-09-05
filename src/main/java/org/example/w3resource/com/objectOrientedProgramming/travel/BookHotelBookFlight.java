643526b2d76994629fe39724800d2a94cdf80908
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-05 22:46:25 +0200

    Object Oriented Programming - BookHotel - prostaaplikacja do zarządzania rezerwacjami

---
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/BookHotelBookFlight.java
@@ -0,0 +1,7 @@
+package org.example.w3resource.com.objectOrientedProgramming.travel;
+
+import java.util.ArrayList;
+
+public class BookHotelBookFlight {
+    ArrayList<Hotel> bookHotel = new ArrayList<>();
+}
