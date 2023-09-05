5ec6e4d483ccd1f20ed9abd99eade183036fba5b
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-05 22:47:55 +0200

    Object Oriented Programming - Main = klasa wywołująca

---
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/Main.java
@@ -0,0 +1,7 @@
+package org.example.w3resource.com.objectOrientedProgramming.travel;
+
+public class Main {
+    public static void main(String[] args) {
+        BookHotelBookFlight app = new BookHotelBookFlight();
+    }
+}
