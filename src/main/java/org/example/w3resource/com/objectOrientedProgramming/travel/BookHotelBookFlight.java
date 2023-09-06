7274ebad8d76fca8b73d1f4a4d47324a2927e878
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-06 21:48:38 +0200

    Object Oriented Programming - Main = pierwsze wywołanie metody

--- src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/BookHotelBookFlight.java
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/BookHotelBookFlight.java
@@ -3,5 +3,15 @@ package org.example.w3resource.com.objectOrientedProgramming.travel;
 import java.util.ArrayList;
 
 public class BookHotelBookFlight {
-    ArrayList<Hotel> bookHotel = new ArrayList<>();
+    private ArrayList<Flight> flights;
+    private ArrayList<Hotel> bookHotel;
+
+    public BookHotelBookFlight() {
+        this.flights = new ArrayList<Flight>();
+        this.bookHotel = new ArrayList<Hotel>();
+    }
+    public void searchFlights(String origin, String destination,String date, int numPassengers){
+        System.out.println("Searching for flight s from " + origin + " to " + destination +
+                " on " + date + " for " + numPassengers + " passengers.");
+    }
 }
--- src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/Main.java
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/Main.java
@@ -3,5 +3,6 @@ package org.example.w3resource.com.objectOrientedProgramming.travel;
 public class Main {
     public static void main(String[] args) {
         BookHotelBookFlight app = new BookHotelBookFlight();
+        app.searchFlights("New York", "London", "2022=09=01",1);
     }
 }
