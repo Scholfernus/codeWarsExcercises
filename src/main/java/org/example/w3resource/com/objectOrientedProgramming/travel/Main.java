5866396f3e84e2fedb47679b8e80bb39e9e5adad
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-07 22:18:19 +0200

    Object Oriented Programming - Main = dodanie metody

--- src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/Main.java
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         BookHotelBookFlight app = new BookHotelBookFlight();
         app.searchFlights("New York", "London", "2022=09=01",1);
+        app.searchHotels("London","2022-08-01","2022-09-05",2);
     }
 }
