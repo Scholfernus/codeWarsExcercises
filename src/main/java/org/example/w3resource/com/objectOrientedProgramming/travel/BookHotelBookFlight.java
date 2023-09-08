4bb8ba96fadefadac8709a2869fc1a8ba2899b1e
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-08 21:47:09 +0200

    Object Oriented Programming - BookHotelBookFlight = dodanie metody

--- src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/BookHotelBookFlight.java
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/BookHotelBookFlight.java
@@ -5,11 +5,11 @@ import java.util.Random;
 
 public class BookHotelBookFlight {
     private ArrayList<Flight> flights;
-    private ArrayList<Hotel> bookHotel;
+    private ArrayList<Hotel> hotels;
 
     public BookHotelBookFlight() {
         this.flights = new ArrayList<Flight>();
-        this.bookHotel = new ArrayList<Hotel>();
+        this.hotels = new ArrayList<Hotel>();
     }
 
     public void searchFlights(String origin, String destination, String date, int numPassengers) {
@@ -28,7 +28,13 @@ public class BookHotelBookFlight {
         this.flights.add(flight);
         System.out.println("Flight booked! Confirmation number: " + confirmationNumber);
     }
-
+    public void bookHotel(int hotelId, String guestName, String location, String checkIn, String checkOut, int numGuests, double price){
+        Hotel hotel = new Hotel(hotelId, guestName, location,checkIn,checkOut,numGuests,price);
+        int confirmationNumber = generateConfirmationNumber();
+        hotel.setConfirmationNumber(confirmationNumber);
+        this.hotels.add(hotel);
+        System.out.println("Hotel booked! Confirmation number: " + confirmationNumber);
+    }
     private int generateConfirmationNumber() {
         Random random = new Random();
         return random.nextInt(900000) + 100000;
