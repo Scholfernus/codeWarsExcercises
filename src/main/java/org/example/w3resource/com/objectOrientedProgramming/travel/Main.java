2717ea2d80cad6a1be165365f31a9cd973dcfba4
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-09-08 21:45:23 +0200

    Object Oriented Programming - Main = dodanie metody

--- src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/Main.java
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/travel/Main.java
@@ -5,5 +5,14 @@ public class Main {
         BookHotelBookFlight app = new BookHotelBookFlight();
         app.searchFlights("New York", "London", "2022=09=01",1);
         app.searchHotels("London","2022-08-01","2022-09-05",2);
+        app.bookFlight(12345678,"John Noodle","New York",
+                "London","2022-09-10",1,688.00);
+        app.bookFlight(356498752,"Jenifer Lopez","Newada",
+                "London","2022-09-11",1,250);
+        app.bookHotel(98564215,"John Noodle","London","2022 - 09 23",
+                "2022-09-30",2,290);
+
+
+
     }
 }
