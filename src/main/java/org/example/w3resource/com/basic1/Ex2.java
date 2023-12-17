bab63451dbc6cad89ee75057a2d2e0164c66771d
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-17 21:54:29 +0100

    Zadania ze strony w3resource - baza1

---
+++ src/main/java/org/example/w3resource/com/basic1/Ex2.java
@@ -0,0 +1,15 @@
+package org.example.w3resource.com.basic1;
+
+public class Ex2 {
+    //Write a Java program to print the sum of two numbers.
+    //Test Data:
+    //74 + 36
+    //Expected Output :
+    //110
+    public static void main(String[] args) {
+        int a = 74;
+        int b = 36;
+        System.out.println("Test Data:\n" + a + "+" + b);
+        System.out.println(a+b);
+    }
+}
---
+++ src/main/java/org/example/w3resource/com/basic1/Ex3.java
@@ -0,0 +1,15 @@
+package org.example.w3resource.com.basic1;
+
+public class Ex3 {
+    //Write a Java program to divide two numbers and print them on the screen.
+    //Test Data :
+    //50/3
+    //Expected Output :
+    //16
+    public static void main(String[] args) {
+        int a = 74;
+        int b = 36;
+        System.out.println("Test Data:\n" + a + ":" + b);
+        System.out.println(a / b);
+    }
+}
