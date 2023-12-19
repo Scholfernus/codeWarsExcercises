ad4e3496efd3e73cbc56fdd9c97ce7d29e01fcd7
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-19 22:26:59 +0100

    Zadania ze strony w3resource - baza1 = ćwiczenia od 31 do 33

---
+++ src/main/java/org/example/w3resource/com/basic1/Ex33.java
@@ -0,0 +1,14 @@
+package org.example.w3resource.com.basic1;
+
+import java.util.Scanner;
+
+public class Ex33 {
+    //Write a Java program and compute the sum of an integer's digits
+    public static void main(String[] args) {
+        System.out.println("Podaj dowolną liczbę wielocyfrową");
+        Scanner number = new Scanner(System.in);
+        int sum = 0;
+        while (sum)
+        number % 10
+    }
+}
