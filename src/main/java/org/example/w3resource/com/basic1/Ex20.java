35d35eeeb1199ae305ed376c5ac68a10e575e4e9
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-19 19:50:44 +0100

    Zadania ze strony w3resource - baza1 = ćwiczenia od 17 do 20

---
+++ src/main/java/org/example/w3resource/com/basic1/Ex20.java
@@ -0,0 +1,35 @@
+package org.example.w3resource.com.basic1;
+
+import java.util.Scanner;
+
+public class Ex20 {
+    //Write a Java program to convert a decimal number to a hexadecimal number.
+    //Input Data:
+    //Input a decimal number: 15
+    public static void main(String[] args) {
+        // Declare variables to store decimal number and remainder
+        int dec_num, rem;
+
+        // Initialize an empty string for the hexadecimal number
+        String hexdec_num = "";
+
+        // Define the hexadecimal number digits
+        char hex[] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+
+        // Create a Scanner object to read input from the user
+        Scanner in = new Scanner(System.in);
+
+        // Prompt the user to input a decimal number
+        System.out.print("Input a decimal number: ");
+        dec_num = in.nextInt();
+
+        // Convert the decimal number to hexadecimal
+        while (dec_num > 0) {
+            rem = dec_num % 16;
+            hexdec_num = hex[rem] + hexdec_num;
+            dec_num = dec_num / 16;
+        }
+        // Display the hexadecimal representation of the decimal number
+        System.out.print("Hexadecimal number is: " + hexdec_num + "\n");
+    }
+}
