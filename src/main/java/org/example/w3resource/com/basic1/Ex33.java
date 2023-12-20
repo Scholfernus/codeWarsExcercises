04788db34a3fdce785709ab61dccaf41b56c3253
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-20 22:08:49 +0100

    Zadania ze strony w3resource - baza1 = ćwiczenia 33 - poprawiony i dobrzedziałający kod z dwoma rozwiązaniami

--- src/main/java/org/example/w3resource/com/basic1/Ex33.java
+++ src/main/java/org/example/w3resource/com/basic1/Ex33.java
@@ -6,9 +6,25 @@ public class Ex33 {
     //Write a Java program and compute the sum of an integer's digits
     public static void main(String[] args) {
         System.out.println("Podaj dowolną liczbę wielocyfrową");
-        Scanner number = new Scanner(System.in);
-        int sum = 0;
-        while (sum)
-        number % 10
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        String value = String.valueOf(number);
+        char[] array = value.toCharArray();
+        int sum1 = 0;
+        for (char digitChar : array) {
+            int value1 = Character.getNumericValue(digitChar);
+            sum1 += value1;
+        }
+        System.out.println(sum1);
+
+        int sum2 = 0;
+
+        // Calculate the sum of the digits
+        while (number != 0) {
+            sum2 += number % 10;
+            number /= 10;
+        }
+        System.out.println(sum2);
     }
 }
+
