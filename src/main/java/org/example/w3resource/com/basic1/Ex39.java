7295ba3d11fdf70f4a78ed86cee5b5d9d9dfc8d5
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-21 22:09:50 +0100

    Zadania ze strony w3resource - baza1 = ćwiczenia 39 jest jeszcze nieskończone - wymaga koncentracji

---
+++ src/main/java/org/example/w3resource/com/basic1/Ex39.java
@@ -0,0 +1,20 @@
+package org.example.w3resource.com.basic1;
+
+import java.util.Random;
+
+public class Ex39 {
+    //Write a Java program to create and display a unique three-digit number using 1, 2, 3, 4.
+    // Also count how many three-digit numbers are there.
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4};
+        Random random = new Random();
+        int []array2 = new int[3];
+        for (int i = 1; i < 3; i++) {
+            int number = random.nextInt(array[i]);
+            array2[i]=number;
+        }
+        for (int number:array2) {
+            System.out.print(number);
+        }
+    }
+}
