4fad8e2ea33bddd95896594315932072c478b8e9
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-22 22:24:31 +0100

    Zadania ze strony w3resource - baza1 = ćwiczenia 60

---
+++ src/main/java/org/example/w3resource/com/basic1/Ex60.java
@@ -0,0 +1,16 @@
+package org.example.w3resource.com.basic1;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class Ex60 {
+    // Write a Java program to find the penultimate (next to the last) word in a sentence.
+    // Input a String: The quick brown fox jumps over the lazy dog.
+    //Penultimate word: lazy
+    public static void main(String[] args) {
+        String findWord = "The quick brown fox jumps over the lazy dog.";
+        String[] split = findWord.split(",");
+        Object[] array = Arrays.stream(split).toArray();
+        System.out.println(array[array.length - 1]);
+    }
+}
