132cd0399c048a9c0bdb7ded03c676511f85bae1
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-23 18:56:06 +0100

    Zadania ze strony w3resource - baza1 = ćwiczenia 61 do 69

---
+++ src/main/java/org/example/w3resource/com/basic1/Ex61.java
@@ -0,0 +1,11 @@
+package org.example.w3resource.com.basic1;
+
+public class Ex61 {
+    // Write a Java program to reverse a word.
+    public static void main(String[] args) {
+        String word = "elcyciB";
+        StringBuilder stringBuilder = new StringBuilder(word);
+        StringBuilder reversed = stringBuilder.reverse();
+        System.out.println(reversed);
+    }
+}
---
+++ src/main/java/org/example/w3resource/com/basic1/Ex69.java
@@ -0,0 +1,14 @@
+package org.example.w3resource.com.basic1;
+
+import java.util.Scanner;
+
+public class Ex69 {
+    //  Write a Java program to extract the first half of a even string
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Put some word: ");
+        String word = scanner.nextLine();
+        int cut = word.length() / 2;
+        System.out.println(word.substring(0, cut));
+    }
+}
