e455d8057fc2dcabacfe5c0ae73d33a9d98fdc72
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-23 18:56:37 +0100

    Zmiana nazwy klasy

--- src/main/java/org/example/w3resource/com/basic1/Ex16.java
+++ src/main/java/org/example/w3resource/com/basic1/Ex16.java
@@ -1,6 +1,6 @@
 package org.example.w3resource.com.basic1;
 
-public class Ec16 {
+public class Ex16 {
     //Write a Java program to print a face
     public static void main(String[] args) {
         System.out.println(" +\"\"\"\"\"+ ");
