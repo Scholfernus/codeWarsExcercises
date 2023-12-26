ee3bdcccf66df7f4aec2cce47bb1a79fe7394ade
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-26 13:28:48 +0100

    Zadania ze strony w3resources.com - kod rozwiązania zad 103 - prawidłowe rozwiązanie

--- src/main/java/org/example/w3resource/com/basic1/from101to150/Ex103.java
+++ src/main/java/org/example/w3resource/com/basic1/from101to150/Ex103.java
@@ -1,20 +1,39 @@
 package org.example.w3resource.com.basic1.from101to150;
 
+import java.util.Arrays;
+
 public class Ex103 {
     // Write a Java program to create an array from a given array of integers.
-// The newly created array will contain elements from the given array after the last element value is 10.
+    // The newly created array will contain elements from the given array after the last element value is 10.
     public static void main(String[] args) {
-        int counter = 0;
-        int[] newArray = new int[]{};
+        int counter = -1;
         int[] numbers = {10, 11, 10, 30, 45, 20, 33, 53};
-        for (int i = 0; i < numbers.length; i++) {
+        System.out.println(Arrays.toString(numbers));
+
+        // Znajdź indeks ostatniego wystąpienia liczby 10
+        for (int i = numbers.length - 1; i >= 0; i--) {
             if (numbers[i] == 10) {
-                counter = numbers[i];
+                counter = i;
+                break;
             }
         }
-        for (int i =counter+1; i < numbers.length-1-counter; i++) {
-            newArray[] = numbers[i];
+
+        // Sprawdź, czy znaleziono liczbę 10
+        if (counter != -1) {
+            // Utwórz nową tablicę o odpowiedniej długości
+            int[] newArray = new int[numbers.length - counter - 1];
+
+            // Przypisz wartości do nowej tablicy
+            for (int i = 0; i < newArray.length; i++) {
+                newArray[i] = numbers[counter + 1 + i];
+            }
+
+            // Wyświetl nową tablicę
+            System.out.println(Arrays.toString(newArray));
+        } else {
+            System.out.println("Brak liczby 10 w tablicy.");
         }
     }
 }
+
 
