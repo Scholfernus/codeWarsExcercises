0d90732b52f04216f33c7b59df823358f54edd8b
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-29 22:01:56 +0100

    Zadania ze strony w3resources.com - kod rozwiązania zad 116 - pełne rozwiązanie

--- src/main/java/org/example/w3resource/com/basic1/from101to150/Ex116.java
+++ src/main/java/org/example/w3resource/com/basic1/from101to150/Ex116.java
@@ -5,6 +5,16 @@ public class Ex116 {
 //    "Fizz" instead of the number and print "Buzz" for five.
 //    When the number is divided by three and five, print "fizz buzz".
     public static void main(String[] args) {
-
+        for (int i = 0; i < 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.print("fizz buzz " + i + ", ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz " + i + ", ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz " + i + ", ");
+            } else {
+                System.out.println("Nie spełnia warunku: " + i);
+            }
+        }
     }
 }
