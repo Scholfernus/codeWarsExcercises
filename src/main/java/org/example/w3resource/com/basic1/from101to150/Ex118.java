152d87bcf30f0b7d22a549ba253d9f991261b38f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-12-29 22:02:32 +0100

    Zadania ze strony w3resources.com - kod rozwiązania zad 118 - pełne rozwiązanie

--- src/main/java/org/example/w3resource/com/basic1/from101to150/Ex118.java
+++ src/main/java/org/example/w3resource/com/basic1/from101to150/Ex118.java
@@ -1,8 +1,22 @@
 package org.example.w3resource.com.basic1.from101to150;
 
+import java.util.Scanner;
+
 public class Ex118 {
     //    Write a Java program to get the first occurrence (Position starts from 0.) of a string within a given string
     public static void main(String[] args) {
-
+        String given = "Zadanie polega na napisaniu programu w języku Java, który znajdzie pierwsze wystąpienie " +
+                "(pozycja zaczyna się od 0.) określonego ciągu znaków w danym ciągu znaków. Innymi słowy, " +
+                "program powinien zlokalizować, na którym indeksie (licząc od 0) " +
+                "w danym ciągu znaków rozpoczyna się pierwsze wystąpienie określonego podciągu znaków.";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Jakiego słowa szukasz? ");
+        String findString = scanner.nextLine();
+        int index = given.indexOf(findString);
+        if (index != -1) {
+            System.out.println("Pierwsze wystąpienie słowa zaczyna się na indeksie: " + index);
+        } else {
+            System.out.println("Słowo nie zostało znalezione w danym ciągu.");
+        }
     }
 }
