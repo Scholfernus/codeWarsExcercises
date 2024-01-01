6161179754aeca9950230aa719d838921596a393
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-01 21:57:19 +0100

    Zadania ze strony w3resources.com - kod rozwiązania zadań 139 - pełne rozwiązanie - ciekawe z komentarzem drugie, równie ciekawe rozwiązanie

--- src/main/java/org/example/w3resource/com/basic1/from101to150/Ex139.java
+++ src/main/java/org/example/w3resource/com/basic1/from101to150/Ex139.java
@@ -27,3 +27,27 @@ public class Ex139 {
         }
     }
 }
+
+//int[] numbers = {1, 2, 3, -6, 5, 4};
+//        Map<Integer, Integer> sumMap = new HashMap<>();
+//        int sum = 0;
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            sum += numbers[i];
+//            if (sum == 0) {
+//                // Znaleziono podtablicę o sumie równiej zero
+//                System.out.println("Index of the subarray: [0, " + i + "]");
+//                return;
+//            }
+//            if (sumMap.containsKey(sum)) {
+//                // Znaleziono sumę wcześniej
+//                int startIndex = sumMap.get(sum) + 1;
+//                int endIndex = i;
+//                System.out.println("Index of the subarray: [" + startIndex + ", " + endIndex + "]");
+//                return;
+//            }
+//            sumMap.put(sum, i);
+//        }
+//
+//        System.out.println("No subarray found with sum zero.");
+//    }
