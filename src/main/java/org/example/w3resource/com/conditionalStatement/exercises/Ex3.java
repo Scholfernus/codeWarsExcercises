4fcafbbbc73c6e2ea971b07cffb854869399d457
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-02 22:11:02 +0100

    Zadania ze strony w3resources.com - kod rozwiązania zadania 3 - pełne rozwiązanie - Java Conditional Statement Exercises

---
+++ src/main/java/org/example/w3resource/com/conditionalStatement/exercises/Ex3.java
@@ -0,0 +1,26 @@
+package org.example.w3resource.com.conditionalStatement.exercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex3 {
+    //    Write a Java program that takes three numbers from the user and prints the greatest number.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj dowolną 1-szą liczbę: ");
+        int numb1 = scanner.nextInt();
+        System.out.println("Podaj dowolną 2-gą liczbę: ");
+        int numb2 = scanner.nextInt();
+        System.out.println("Podaj dowolną 3-cią liczbę: ");
+        int numb3 = scanner.nextInt();
+        int greatest = numb1;
+        if (numb2>greatest){
+            greatest=numb2;
+        }
+        if (numb3>greatest){
+            greatest=numb3;
+        }
+        System.out.println("The greatest: " + greatest);
+    }
+}
