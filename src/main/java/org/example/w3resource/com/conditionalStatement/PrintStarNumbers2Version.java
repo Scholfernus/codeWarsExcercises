1c5796a7c17024e31504385ba7460af500abfb62
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-02 22:12:40 +0100

    Zmiana nazwy klasy

--- src/main/java/org/example/w3resource/com/conditionalStatement/PrintStarNumbers2Version.java
+++ src/main/java/org/example/w3resource/com/conditionalStatement/PrintStarNumbers2Version.java
@@ -1,6 +1,6 @@
 package org.example.w3resource.com.conditionalStatement;
 
-public class PrintStarNUmbers2Version {
+public class PrintStarNumbers2Version {
     public static void main(String[] args) {
         for (int i = 1; i <=4; i++) {
             for (int j = 1; j <=i; j++) {
