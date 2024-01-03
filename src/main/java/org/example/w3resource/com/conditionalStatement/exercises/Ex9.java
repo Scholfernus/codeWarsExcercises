469ed21475aed8933457847d68770a270acf1393
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-03 21:35:18 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 9

---
+++ src/main/java/org/example/w3resource/com/conditionalStatement/exercises/Ex9.java
@@ -0,0 +1,8 @@
+package org.example.w3resource.com.conditionalStatement.exercises;
+
+public class Ex9 {
+    //    Write a Java program that takes a year from the user and prints whether it is a leap year or not.
+    public static void main(String[] args) {
+
+    }
+}
