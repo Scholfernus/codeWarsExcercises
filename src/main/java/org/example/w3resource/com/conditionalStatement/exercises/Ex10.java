8ac353191b9b5ee79e4f2fd2de27733ad309ed65
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-04 22:30:52 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 10

---
+++ src/main/java/org/example/w3resource/com/conditionalStatement/exercises/Ex10.java
@@ -0,0 +1,10 @@
+package org.example.w3resource.com.conditionalStatement.exercises;
+
+public class Ex10 {
+    //    Write a Java program to display the first 10 natural numbers.
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
