a7bf44b1d386689ddc3b2b2af1359de5186134eb
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-05 21:44:39 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 1 - metody rekursywne

---
+++ src/main/java/org/example/w3resource/com/recursiveMethods/Ex1.java
@@ -0,0 +1,25 @@
+package org.example.w3resource.com.recursiveMethods;
+
+import java.util.Scanner;
+
+public class Ex1 {
+    //    Write a Java recursive method to calculate the factorial of a given positive integer
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj dodatnią liczbę całkowitą: ");
+        int number = scanner.nextInt();
+        if (number >= 0) {
+            long factorial = calculateFactorial(number);
+            System.out.println("Silnia z " + number + " to: " + factorial);
+        } else {
+            System.out.println("Podana liczba nie jest dodatnia.");
+        }
+    }
+    private static long calculateFactorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+}
