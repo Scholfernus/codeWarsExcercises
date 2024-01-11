76d58185967a63bf6d431e1c0f1b0cf5d143852a
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-11 21:48:31 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 3 arrays

---
+++ src/main/java/org/example/w3resource/com/array/exercices/Ex3.java
@@ -0,0 +1,23 @@
+package org.example.w3resource.com.array.exercices;
+
+public class Ex3 {
+    //    Write a Java program to print the following grid.
+    //- - - - - - - - - -
+    //- - - - - - - - - -
+    //- - - - - - - - - -
+    //- - - - - - - - - -
+    //- - - - - - - - - -
+    //- - - - - - - - - -
+    //- - - - - - - - - -
+    //- - - - - - - - - -
+    //- - - - - - - - - -
+    //- - - - - - - - - -
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                System.out.print(" - ");
+            }
+            System.out.println();
+        }
+    }
+}
