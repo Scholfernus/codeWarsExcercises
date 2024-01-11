6f9b715ebe69b452dbd67b2b1779d6c798ca9b7c
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-11 21:47:27 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 6 i 7 exeptions - koniecznie do skończenia

---
+++ src/main/java/org/example/w3resource/com/exceptions/exercises/Ex6.java
@@ -0,0 +1,8 @@
+package org.example.w3resource.com.exceptions.exercises;
+
+public class Ex6 {
+    //    Write a Java program that reads a list of integers from the user and throws an exception if any numbers are duplicates.
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/example/w3resource/com/exceptions/exercises/Ex7.java
@@ -0,0 +1,9 @@
+package org.example.w3resource.com.exceptions.exercises;
+
+public class Ex7 {
+    //    Write a Java program to create a method that takes a string as input and throws an exception
+//    if the string does not contain vowels.
+    public static void main(String[] args) {
+
+    }
+}
