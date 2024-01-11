2e814d8f0dbc25ea9ab4a8a0aa1d363cee14c5c2
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-11 21:49:20 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 5 exeptions

--- src/main/java/org/example/w3resource/com/exceptions/exercises/Ex5.java
+++ src/main/java/org/example/w3resource/com/exceptions/exercises/Ex5.java
@@ -1,7 +1,5 @@
 package org.example.w3resource.com.exceptions.exercises;
 
-import org.example.w3resource.com.exceptions.FileEmptyException;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -10,16 +8,29 @@ public class Ex5 {
     //    Write a Java program that reads a file and throws an exception if the file is empty.
     public static void main(String[] args) {
         try {
-            File file = new File("Text.txt");
-            Scanner scanner = new Scanner(file);
-            if (!scanner.hasNextLine()) {
-            scanner.close();
-            throw new FileEmptyException("File is empty");
-            }
-        } catch (FileEmptyException e) {
-            System.out.println(e.getMessage());
+            checkFileNotEmpty("test1.txt");
+            System.out.println("File is not empty.");
         } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+            System.out.println("Error: " + e.getMessage());
+        } catch (EmptyFileException e) {
+            System.out.println("Error: " + e.getMessage());
         }
+    }
+
+    public static void checkFileNotEmpty(String fileName) throws FileNotFoundException, EmptyFileException {
+        File file = new File(fileName);
+        Scanner scanner = new Scanner(file);
+
+        if (!scanner.hasNextLine()) {
+            scanner.close();
+            throw new EmptyFileException("File is empty.");
+        }
+
+        scanner.close();
+    }
+}
+class EmptyFileException extends Exception {
+    public EmptyFileException(String message) {
+        super(message);
     }
 }
