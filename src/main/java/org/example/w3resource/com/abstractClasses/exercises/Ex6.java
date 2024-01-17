41edb12afa9be143bccb250e62ba3f742869168f
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-17 22:14:18 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadanie 6 abstract class

---
+++ src/main/java/org/example/w3resource/com/abstractClasses/exercises/Ex6.java
@@ -0,0 +1,56 @@
+package org.example.w3resource.com.abstractClasses.exercises;
+
+public class Ex6 {
+    //    Write a Java program to create an abstract class Shape3D with abstract methods calculateVolume()
+    //    and calculateSurfaceArea(). Create subclasses Sphere and Cube that extend the Shape3D class and implement
+    //    the respective methods to calculate the volume and surface area of each shape.
+    public static void main(String[] args) {
+        Shape3D sphere = new Sphere(7.0);
+        Shape3D cube = new Cube(6.0);
+
+        System.out.println("Sphere Volume: " + sphere.calculateVolume());
+        System.out.println("Sphere Surface Area: " + sphere.calculateSurfaceArea());
+
+        System.out.println("Cube Volume: " + cube.calculateVolume());
+        System.out.println("Cube Surface Area: " + cube.calculateSurfaceArea());
+    }
+}
+abstract class Shape3D {
+    public abstract double calculateVolume();
+
+    public abstract double calculateSurfaceArea();
+}
+class Sphere extends Shape3D {
+    private double radius;
+
+    public Sphere(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateVolume() {
+        return (4.0 / 3.0) * Math.PI * Math.pow(radius, 3);
+    }
+
+    @Override
+    public double calculateSurfaceArea() {
+        return 4 * Math.PI * Math.pow(radius, 2);
+    }
+}
+class Cube extends Shape3D {
+    private final double sideLength;
+
+    public Cube(double sideLength) {
+        this.sideLength = sideLength;
+    }
+
+    @Override
+    public double calculateVolume() {
+        return Math.pow(sideLength, 3);
+    }
+
+    @Override
+    public double calculateSurfaceArea() {
+        return 6 * Math.pow(sideLength, 2);
+    }
+}
