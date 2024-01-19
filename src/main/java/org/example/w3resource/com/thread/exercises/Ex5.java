ff816263b3f18bb51a68f4985df8ceb73e71925d
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-19 22:18:31 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 5 Thread - not finished

---
+++ src/main/java/org/example/w3resource/com/thread/exercises/Ex5.java
@@ -0,0 +1,5 @@
+package org.example.w3resource.com.thread.exercises;
+
+public class Ex5 {
+//    Write a Java program that calculates the sum of all prime numbers up to a given limit using multiple threads
+}
