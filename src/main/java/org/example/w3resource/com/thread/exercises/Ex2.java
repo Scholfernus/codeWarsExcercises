a1b03cf964f24358507d61d2d6fc746c2b2595b4
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-19 22:15:17 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 2 Thread

---
+++ src/main/java/org/example/w3resource/com/thread/exercises/Ex2.java
@@ -0,0 +1,47 @@
+package org.example.w3resource.com.thread.exercises;
+
+public class Ex2 extends Thread {
+    //    Write a Java program that creates two threads to find and print even and odd numbers from 1 to 20.
+    private static final int MAX_NUMBER = 20;
+    private static Object lock = new Object();
+    private static boolean isEvenTurn = true;
+
+    public static void main(String[] args) {
+        Thread evenThread = new Thread(() -> {
+            for (int i = 2; i <= MAX_NUMBER; i += 2) {
+                synchronized(lock) {
+                    while (!isEvenTurn) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Even Number from evenThread: " + i);
+                    isEvenTurn = false;
+                    lock.notify();
+                }
+            }
+        });
+
+        Thread oddThread = new Thread(() -> {
+            for (int i = 1; i <= MAX_NUMBER; i += 2) {
+                synchronized(lock) {
+                    while (isEvenTurn) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println("Odd Number from oddThread: " + i);
+                    isEvenTurn = true;
+                    lock.notify();
+                }
+            }
+        });
+
+        evenThread.start();
+        oddThread.start();
+    }
+}
