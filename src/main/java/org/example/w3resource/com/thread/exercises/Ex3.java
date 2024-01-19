ec840cc22df738c81c7bb5c7551ce53d8e58b546
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-19 22:18:05 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 3 Thread - not finished

---
+++ src/main/java/org/example/w3resource/com/thread/exercises/Ex3.java
@@ -0,0 +1,8 @@
+package org.example.w3resource.com.thread.exercises;
+
+public class Ex3 {
+    //    Write a Java program that sorts an array of integers using multiple threads.
+    public static void main(String[] args) {
+
+    }
+}
