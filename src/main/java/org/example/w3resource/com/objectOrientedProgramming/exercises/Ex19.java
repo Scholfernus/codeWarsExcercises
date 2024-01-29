9161ca8617602579d81c2af587a69f326e876e96
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-29 19:47:03 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 19 OOP

---
+++ src/main/java/org/example/w3resource/com/objectOrientedProgramming/exercises/Ex19.java
@@ -0,0 +1,9 @@
+package org.example.w3resource.com.objectOrientedProgramming.exercises;
+
+public class Ex19 {
+    //    Write a Java program to create a class with methods to search for flights and hotels, and to book and cancel
+//    reservations.
+    public static void main(String[] args) {
+
+    }
+}
