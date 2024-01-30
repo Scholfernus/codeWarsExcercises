63f449fa49bb151813f112e9433e39e81d668a04
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-30 19:27:46 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 6 Interfaces - kod nie jest skończony

---
+++ src/main/java/org/example/w3resource/com/interfaceClass/exercises/Ex6.java
@@ -0,0 +1,33 @@
+package org.example.w3resource.com.interfaceClass.exercises;
+
+public class Ex6 {
+    //    Write a Java program to create an interface Drawable with a method draw() that takes no arguments and returns void.
+//    Create three classes Circle, Rectangle, and Triangle that implement the Drawable interface and override the draw()
+//    method to draw their respective shapes.
+    public static void main(String[] args) {
+
+    }
+}
+interface Drawable{
+    void draw();
+}
+class Kolo implements Drawable{
+
+    @Override
+    public void draw() {
+
+    }
+}
+class Prostokat implements Drawable{
+
+    @Override
+    public void draw() {
+
+    }
+}
+class Trojkat2 implements Drawable{
+    @Override
+    public void draw() {
+
+    }
+}
