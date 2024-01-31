4efaf9bbb206d6cdd1290858741a836acecebd4c
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2024-01-31 21:34:43 +0100

    Zadania ze strony w3resources.com - rozwiązanie zadania 1 Encapsulation

---
+++ src/main/java/org/example/w3resource/com/encapsulation/Ex1.java
@@ -0,0 +1,49 @@
+package org.example.w3resource.com.encapsulation;
+
+public class Ex1 {
+    //    Write a Java program to create a class called Person with private instance variables name, age. and country.
+//    Provide public getter and setter methods to access and modify these variables.
+    public static void main(String[] args) {
+    Person person = new Person();
+        person.setName("Barthel Viktorija");
+        person.setAge(25);
+        person.setCountry("USA");
+
+        String name = person.getName();
+        int age = person.getAge();
+        String country = person.getCountry();
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Country: " + country);
+    }
+}
+class Person{
+    private String name;
+    private int age;
+    private String country;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
